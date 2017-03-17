b5d6548e4bc59cdbae3936198c873ed80c692a9b
Jerry <jianglei_fd@qq.com>
2017-03-17 15:25:23 +0800

    添加配置文件； 修改pom文件

---
+++ config-server/src/main/java/com/demo/eidea/config/server/main/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.demo.eidea.config.server.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by jiangl-e on 2017/3/17.
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
