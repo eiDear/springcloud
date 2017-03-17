da58d4339fbb256188cae6e99729cb88d6300265
Jerry <jianglei_fd@qq.com>
2017-03-17 16:06:43 +0800

    添加和修改配置文件； 修改pom文件

--- config-client/src/main/java/com/glodon/eidea/config/client/ConfigClientApplication.java
+++ config-client/src/main/java/com/glodon/eidea/config/client/ConfigClientApplication.java
@@ -1,17 +1,15 @@
-package com.demo.eidea.config.server.main;
+package com.glodon.eidea.config.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Created by jiangl-e on 2017/3/17.
  */
 @SpringBootApplication
-@EnableConfigServer
-public class ConfigServerApplication {
+public class ConfigClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
+        SpringApplication.run(ConfigClientApplication.class, args);
     }
 }
---
+++ config-client/src/main/java/com/glodon/eidea/config/client/config/Configuration.java
@@ -0,0 +1,27 @@
+package com.glodon.eidea.config.client.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by jiangl-e on 2017/3/17.
+ */
+@RestController
+@RefreshScope
+public class Configuration {
+    @Value("${app.message}")
+    private String message;
+
+    @Value("${user.username}")
+    private String username;
+
+    @Value("${user.userage}")
+    private String userage;
+
+    @RequestMapping("/test")
+    public String test(){
+        return message + " >> " + username + " >> " + userage;
+    }
+}
